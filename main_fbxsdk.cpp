16ddf0fec5f9b2c389647c7a55c7a1e5745ac45d
Aras Pranckevicius <aras@nesnausk.org>
2025-05-04 13:12:26 +0300

    ufbx: parse multiple files in parallel

--- main_fbxsdk.cpp
+++ main_fbxsdk.cpp
@@ -1,5 +1,7 @@
 #include <fbxsdk.h>
 #include <chrono>
+#include <vector>
+
 
 /*
 test_fbxsdk.exe 4505 KB
@@ -21,6 +23,8 @@ Skeletons: 3495
 
 struct Stats
 {
+    const char* path = nullptr;
+    float ms = 0;
     size_t meshes = 0;
     size_t lights = 0;
     size_t cameras = 0;
@@ -69,44 +73,61 @@ static void walk_node(FbxNode* node, Stats &r_stats)
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        printf("Usage: pass input FBX file on the command line\n");
+        printf("Usage: pass input FBX files on the command line\n");
         return 1;
     }
-    const char* input_filename = argv[1];
-    printf("Loading %s...\n", input_filename);
+    const int input_file_count = argc - 1;
+    printf("Loading %i input files (NOT parallel):\n", input_file_count);
+    std::vector<Stats> stats(input_file_count);
+    for (int i = 0; i < input_file_count; i++)
+    {
+        stats[i].path = argv[i + 1];
+        printf("- '%s'\n", stats[i].path);
+    }
 
+    // Process the files.
+    // Note: you would think that you can do multi-threaded processing of FBX files,
+    // by having each thread create its own FbxManager. But somewhere deep within
+    // FBX SDK, it does use some global/shared state even with separate FbxManagers,
+    // and you will get random crashes :(
     auto t0 = std::chrono::high_resolution_clock::now();
+    for (Stats& st : stats)
+    {
+        auto ft0 = std::chrono::high_resolution_clock::now();
+        // Create SDK manager, IO settings and a scene
+        FbxManager* sdk = FbxManager::Create();
+        FbxIOSettings* ios = FbxIOSettings::Create(sdk, IOSROOT);
+        sdk->SetIOSettings(ios);
+        FbxScene* scene = FbxScene::Create(sdk, "myScene");
 
-    // Create SDK manager, IO settings and a scene
-    FbxManager* sdk = FbxManager::Create();
-    FbxIOSettings* ios = FbxIOSettings::Create(sdk, IOSROOT);
-    sdk->SetIOSettings(ios);
-    FbxScene* scene = FbxScene::Create(sdk, "myScene");
+        // Import the file
+        FbxImporter* importer = FbxImporter::Create(sdk, "");
+        if (!importer->Initialize(st.path, -1, sdk->GetIOSettings())) {
+            printf("Call to FbxImporter::Initialize() failed.\n");
+            printf("Error returned: %s\n\n", importer->GetStatus().GetErrorString());
+            exit(1);
+        }
+        importer->Import(scene);
+        importer->Destroy();
 
-    // Import the file
-    FbxImporter* importer = FbxImporter::Create(sdk, "");
-    if (!importer->Initialize(input_filename, -1, sdk->GetIOSettings())) {
-        printf("Call to FbxImporter::Initialize() failed.\n");
-        printf("Error returned: %s\n\n", importer->GetStatus().GetErrorString());
-        return 1;
-    }
-    importer->Import(scene);
-    importer->Destroy();
+        auto ft1 = std::chrono::high_resolution_clock::now();
+        std::chrono::duration<float, std::milli> fdt = ft1 - ft0;
+        st.ms = fdt.count();
+
+        // Gather some basic stats about the scene
+        FbxNode* root = scene->GetRootNode();
+        walk_node(root, st);
+        sdk->Destroy();
+    };
 
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float, std::milli> dt = t1 - t0;
-    printf("Loaded in %.1f ms\n", dt.count());
+    printf("Done in %.1f ms\n", dt.count());
+    for (const Stats& st : stats)
+    {
+        printf("- '%s' in %.1f ms\n", st.path, st.ms);
+        printf("  - %zi meshes (%.1f Kverts, %.1f Kfaces), %zi lights, %zi cameras, %zi bones\n", st.meshes, st.vertices / 1024.0, st.faces / 1024.0, st.lights, st.cameras, st.skeletons);
+    }
 
-    // Gather some basic stats about the scene
-    Stats stats;
-    FbxNode* root = scene->GetRootNode();
-    walk_node(root, stats);
-    printf("Meshes: %zi (%.1f Kverts, %.1f Kfaces)\n", stats.meshes, stats.vertices / 1024.0, stats.faces / 1024.0);
-    printf("Lights: %zi\n", stats.lights);
-    printf("Cameras: %zi\n", stats.cameras);
-    printf("Skeletons: %zi\n", stats.skeletons);
-
-    // Cleanup
-    sdk->Destroy();
     return 0;
 }
--- main_ufbx.cpp
+++ main_ufbx.cpp
@@ -1,5 +1,7 @@
 #include "external/ufbx/ufbx.h"
 #include <chrono>
+#include <vector>
+#include <execution>
 
 /*
 test_fbxsdk.exe 477 KB
@@ -17,10 +19,30 @@ Meshes: 93 (62.6 Kverts, 59.3 Kfaces)
 Lights: 7
 Cameras: 1
 Skeletons: 3495
+
+Loading 5 input files in parallel:
+- 'MEASURE_ONE.fbx'
+- 'rain_restaurant_bl43.fbx'
+- 'atvi_caldera_from_usd_bl45.fbx'
+- 'intel_moore_lane_exported_from_usd_bl45.fbx'
+- 'blender30splash_noanim_bl43.fbx'
+Done in 2563.9 ms
+- 'MEASURE_ONE.fbx' in 1426.9 ms
+  - 7293 meshes (3053.2 Kverts, 5732.4 Kfaces), 0 lights, 1 cameras, 0 bones
+- 'rain_restaurant_bl43.fbx' in 1193.1 ms
+  - 93 meshes (62.6 Kverts, 59.3 Kfaces), 7 lights, 1 cameras, 3495 bones
+- 'atvi_caldera_from_usd_bl45.fbx' in 2234.6 ms
+  - 4371 meshes (9751.9 Kverts, 16740.1 Kfaces), 0 lights, 13 cameras, 113 bones
+- 'intel_moore_lane_exported_from_usd_bl45.fbx' in 2532.9 ms
+  - 605 meshes (10090.7 Kverts, 10286.1 Kfaces), 51 lights, 1 cameras, 0 bones
+- 'blender30splash_noanim_bl43.fbx' in 1407.4 ms
+  - 431 meshes (2162.6 Kverts, 2090.9 Kfaces), 6 lights, 1 cameras, 4121 bones
 */
 
 struct Stats
 {
+    const char* path = nullptr;
+    float ms = 0;
     size_t meshes = 0;
     size_t lights = 0;
     size_t cameras = 0;
@@ -32,43 +54,56 @@ struct Stats
 int main(int argc, char** argv)
 {
     if (argc < 2) {
-        printf("Usage: pass input FBX file on the command line\n");
+        printf("Usage: pass input FBX files on the command line\n");
         return 1;
     }
-    const char* input_filename = argv[1];
-    printf("Loading %s...\n", input_filename);
+    const int input_file_count = argc - 1;
+    printf("Loading %i input files in parallel:\n", input_file_count);
+    std::vector<Stats> stats(input_file_count);
+    for (int i = 0; i < input_file_count; i++)
+    {
+        stats[i].path = argv[i + 1];
+        printf("- '%s'\n", stats[i].path);
+    }
 
+    // Process the files
     auto t0 = std::chrono::high_resolution_clock::now();
+    std::for_each(std::execution::par, stats.begin(), stats.end(),
+        [](Stats& stats) {
+            auto ft0 = std::chrono::high_resolution_clock::now();
 
-    // Load the file
-    ufbx_load_opts opts = {};
-    ufbx_error error = {};
-    ufbx_scene* scene = ufbx_load_file(input_filename, &opts, &error);
-    if (scene == nullptr) {
-        printf("ERROR: failed to load fbx %s\n", input_filename);
-        return 1;
-    }
+            // Load the file
+            ufbx_load_opts opts = {};
+            ufbx_error error = {};
+            ufbx_scene* scene = ufbx_load_file(stats.path, &opts, &error);
+            if (scene == nullptr) {
+                printf("ERROR: failed to load fbx %s\n", stats.path);
+                exit(1);
+            }
 
+            auto ft1 = std::chrono::high_resolution_clock::now();
+            std::chrono::duration<float, std::milli> fdt = ft1 - ft0;
+            stats.ms = fdt.count();
+
+            // Gather some basic stats about the scene
+            stats.meshes = scene->meshes.count;
+            stats.cameras = scene->cameras.count;
+            stats.lights = scene->lights.count;
+            stats.skeletons = scene->bones.count;
+            for (const ufbx_mesh* mesh : scene->meshes) {
+                stats.vertices += mesh->num_vertices;
+                stats.faces += mesh->num_faces;
+            }
+            ufbx_free_scene(scene);
+        });
     auto t1 = std::chrono::high_resolution_clock::now();
     std::chrono::duration<float, std::milli> dt = t1 - t0;
-    printf("Loaded in %.1f ms\n", dt.count());
-
-    // Gather some basic stats about the scene
-    Stats stats;
-    stats.meshes = scene->meshes.count;
-    stats.cameras = scene->cameras.count;
-    stats.lights = scene->lights.count;
-    stats.skeletons = scene->bones.count;
-    for (const ufbx_mesh* mesh : scene->meshes) {
-        stats.vertices += mesh->num_vertices;
-        stats.faces += mesh->num_faces;
+    printf("Done in %.1f ms\n", dt.count());
+    for (const Stats& st : stats)
+    {
+        printf("- '%s' in %.1f ms\n", st.path, st.ms);
+        printf("  - %zi meshes (%.1f Kverts, %.1f Kfaces), %zi lights, %zi cameras, %zi bones\n", st.meshes, st.vertices / 1024.0, st.faces / 1024.0, st.lights, st.cameras, st.skeletons);
     }
-    printf("Meshes: %zi (%.1f Kverts, %.1f Kfaces)\n", stats.meshes, stats.vertices / 1024.0, stats.faces / 1024.0);
-    printf("Lights: %zi\n", stats.lights);
-    printf("Cameras: %zi\n", stats.cameras);
-    printf("Skeletons: %zi\n", stats.skeletons);
 
-    // Cleanup
-    ufbx_free_scene(scene);
     return 0;
 }
