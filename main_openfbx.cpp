851be465dfe2c4a6a7b120153bca2795a550b5b7
Aras Pranckevicius <aras@nesnausk.org>
2025-05-04 21:40:56 +0300

    Add OpenFBX test

---
+++ main_openfbx.cpp
@@ -0,0 +1,88 @@
+#include <vector>
+#include "common.h"
+
+#include "external/OpenFBX/ofbx.h"
+
+#define IMPORT_FILES_IN_PARALLEL
+
+#if defined(IMPORT_FILES_IN_PARALLEL)
+#include <execution>
+#endif
+
+static void process_file(Stats& entry)
+{
+    auto t0 = time_now();
+
+    // Read the file
+    FILE* fp = fopen(entry.path, "rb");
+    if (!fp) {
+        printf("ERROR: failed to read file %s\n", entry.path);
+        exit(1);
+    }
+    fseek(fp, 0, SEEK_END);
+    const long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    ofbx::u8* file_data = new ofbx::u8[file_size];
+    fread(file_data, 1, file_size, fp);
+    fclose(fp);
+
+    // Load the file
+    ofbx::IScene* scene = ofbx::load(file_data, file_size, 0);
+    if (scene == nullptr) {
+        printf("ERROR: failed to load fbx %s\n", ofbx::getError());
+        exit(1);
+    }
+
+    // Gather some basic stats about the scene
+    entry.meshes = scene->getGeometryCount();
+    entry.cameras = scene->getCameraCount();
+    entry.lights = scene->getLightCount();
+    for (int idx = 0, n = scene->getAllObjectCount(); idx != n; idx++)
+    {
+        const ofbx::Object* obj = scene->getAllObjects()[idx];
+        if (obj->getType() == ofbx::Object::Type::LIMB_NODE)
+            entry.bones++;
+    }
+
+    for (int idx = 0; idx < entry.meshes; idx++)
+    {
+        const ofbx::Geometry* geo = scene->getGeometry(idx);
+        entry.vertices += geo->getGeometryData().getPositions().count;
+        for (int pi = 0, np = geo->getGeometryData().getPartitionCount(); pi != np; pi++) {
+            entry.faces += geo->getGeometryData().getPartition(pi).polygon_count;
+        }
+    }
+    scene->destroy();
+
+    entry.ms = time_duration_ms(t0);
+}
+
+int main(int argc, char** argv)
+{
+    if (argc < 2) {
+        printf("Usage: pass input FBX files on the command line\n");
+        return 1;
+    }
+    const int input_file_count = argc - 1;
+    printf("Loading %i input files in parallel:\n", input_file_count);
+    std::vector<Stats> stats(input_file_count);
+    for (int i = 0; i < input_file_count; i++)
+    {
+        stats[i].path = argv[i + 1];
+        printf("- '%s'\n", stats[i].path);
+    }
+
+    // Process the files
+    auto t0 = time_now();
+
+#if defined(IMPORT_FILES_IN_PARALLEL)
+    std::for_each(std::execution::par, stats.begin(), stats.end(), [](Stats& st) { process_file(st); });
+#else
+    for (Stats& st : stats) { process_file(st); }
+#endif
+    float dt = time_duration_ms(t0);
+    printf("Done in %.1f s\n", dt / 1000.0);
+    Stats sum_stats = aggregate_stats(stats.size(), stats.data());
+    print_sum_stats(sum_stats);
+    return 0;
+}
--- readme.md
+++ readme.md
@@ -2,22 +2,24 @@
 
 ### Libraries
 
-* `fbxsdk`: [Autodesk FBX SDK](https://aps.autodesk.com/developer/overview/fbx-sdk), 2020.3.7.
-* `ufbx`: https://github.com/ufbx/ufbx v0.18.2 (2025 Apr, 5b5494b). MIT or Public Domain.
+* ufbx: https://github.com/ufbx/ufbx (v0.18.2, 2025 Apr, 5b5494b). MIT or Public Domain license.
+* FBX SDK: [Autodesk FBX SDK](https://aps.autodesk.com/developer/overview/fbx-sdk), 2020.3.7.
+* OpenFBX: https://github.com/nem0/OpenFBX (2024 Dec, 82a43d9). MIT license.
 
 
 ### Performance
 
-| Parser                           | Import time, sec | Executable size, KB | Exe size minimal, KB |
-|----------------------------------|---:|---:|---:|
-| FBX SDK                          | 565.4 | 4508 | - |
-| ufbx sequential                  |   7.9 | 479 | 440 |
-| ufbx parallel                    |   2.6 | 496 | 457 |
-| ufbx parallel + internal threads |   2.1 | 501 | 463 |
+| Parser                   | Time sequential, sec | Time parallel, sec | Executable size, KB |
+|--------------------------|------:|-------:|-----:|
+| ufbx                     |   7.9 |    2.6 |  457 |
+| ufbx w/ internal threads |   3.2 |    2.1 |  463 |
+| FBX SDK                  | 565.4 | crash! | 4508 |
+| OpenFBX                  |  19.7 |   12.6 |  316 |
 
-"Minimal" build configuration is stripping out / disabling various features that we are not interested in.
-- ufbx: `UFBX_NO_SUBDIVISION`, `UFBX_NO_TESSELLATION`, `UFBX_NO_GEOMETRY_CACHE`, `UFBX_NO_SCENE_EVALUATION`,
-  `UFBX_NO_SKINNING_EVALUATION`, `UFBX_NO_ANIMATION_BAKING`, `UFBX_NO_TRIANGULATION`,
-  `UFBX_NO_INDEX_GENERATION`, `UFBX_NO_FORMAT_OBJ`.
+Note that `ufbx` is built with various "not interesting for me" parts of it disabled. With all default
+settings, they all would increase the executable size by about 40 KB. The build flags are:
+`UFBX_NO_SUBDIVISION`, `UFBX_NO_TESSELLATION`, `UFBX_NO_GEOMETRY_CACHE`, `UFBX_NO_SCENE_EVALUATION`,
+`UFBX_NO_SKINNING_EVALUATION`, `UFBX_NO_ANIMATION_BAKING`, `UFBX_NO_TRIANGULATION`,
+`UFBX_NO_INDEX_GENERATION`, `UFBX_NO_FORMAT_OBJ`.
 
 Expected: `- 12793 meshes (25120.9 Kverts, 34908.7 Kfaces), 64 lights, 17 cameras, 7729 bones`
